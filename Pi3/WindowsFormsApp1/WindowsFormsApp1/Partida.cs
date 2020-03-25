fb63ead0d08c5692e9a9d623361f879a2b5e049d
danilojpl <danilo.duartejp@outlook.com>
2020-03-25 14:43:28 -0300

    correção dll

---
+++ Pi3/WindowsFormsApp1/WindowsFormsApp1/Partida.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Partida
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string data { get; set; }
+        public string status { get; set; }
+
+        public string getAtributo (string partida, int pos)
+        {
+            string [] atributos = partida.Split(',');
+            return atributos[pos];
+        }
+
+
+    }
+}
